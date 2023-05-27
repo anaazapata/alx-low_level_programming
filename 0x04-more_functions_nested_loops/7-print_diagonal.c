079ee62ec223e411a811295839b23c537833d07a
anaazapata <cecizs1@hotmail.com>
2023-05-26 18:13:10 -0700

    print diagonal to the stdout

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,28 @@
 #include "main.h"
 /**
  * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character should be printed
+ * @n: number of times the character \ should be printed
  * Return: Always 0
  */
 void print_diagonal(int n)
 {
-	int space, diagonal;
+	int row, spaces;
 
 	if (n <= 0)
 	{
 		_putchar(10);
 	}
-	
-	for (diagonal = 1; diagonal <= n; diagonal++)
+	else
 	{
-		for (space = 0; space <= diagonal; space++)
+		for (row = 1; row <= n; row++)
 		{
-			_putchar(' ');
-		}
-		
-		_putchar('\\');
-		_putchar(10);
+			for (spaces = 0; spaces <= row; spaces++)
+			{
+				_putchar(' ');
+			}
 
+			_putchar('\\');
+			_putchar(10);
+		}
 	}
 }
