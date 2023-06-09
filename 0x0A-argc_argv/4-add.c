e974e298875e93ba67430b983d350a8642656303
anaazapata <cecizs1@hotmail.com>
2023-06-09 11:59:04 -0700

    adds positive numbers, if one of the number contains symbols that are not digits print Error and return 1

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num, n, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (n = 0; argv[num][n] != 0; n++)
+		{
+			if (argv[num][n] < 48 || argv[num][n] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
