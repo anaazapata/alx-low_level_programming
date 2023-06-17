aaecb3346ccf34780e3193c705036f0563703f9a
anaazapata <cecizs1@hotmail.com>
2023-06-17 14:57:44 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; && src[i] != 0; i++)
+	for (i = 0; i < n && src[i] != 0; i++)
 	{
 		dest[i] = src[i];
 	}
@@ -18,7 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 	while (i < n)
 	{
 		dest[i] = 0;
-		i++:
+		i++;
 	}
 
 	return (dest);
