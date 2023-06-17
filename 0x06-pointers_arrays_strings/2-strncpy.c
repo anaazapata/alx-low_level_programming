7b12239e63ec768401bb44dda1c44216f6a7fdd6
anaazapata <cecizs1@hotmail.com>
2023-06-17 14:51:21 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * char *_strncpy - copies a string
+ * @dest: dest string
+ * @src: org string
+ * @n: bytes
+ * Return: Always 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; && src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = 0;
+		i++:
+	}
+
+	return (dest);
+}
