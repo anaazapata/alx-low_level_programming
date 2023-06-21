9f1095700ba4139272c31fdab19012e74c09184d
anaazapata <cecizs1@hotmail.com>
2023-06-21 13:34:14 -0700

    calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - returns the sum of a and b
+ * @a: number to sum
+ * @b: number to sum
+ * Return: sum of a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: number to subtract
+ * @b: number to subtract
+ * Return: the substraction of a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b
+ * @a: number to be multiplied
+ * @b: number to be multiplied
+ * Return: product of a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - returns the division of a and b
+ * @a: number to be divided
+ * @b: number to be divided
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the reminder of the division of a number
+ * @a: number
+ * @b: number
+ * Return: reminder of number
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
