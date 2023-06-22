4a6ab7acec5c81aa8952766fd443836e79462a56
anaazapata <cecizs1@hotmail.com>
2023-06-22 15:35:28 -0700

    print all numbers followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @separator: pointer to char
+ * @n: number
+ * Return: Always 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	if (separator == NULL)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
