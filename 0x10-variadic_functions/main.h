274450e994363d0759f0f787ea8312082b595198
anaazapata <cecizs1@hotmail.com>
2023-06-22 15:03:39 -0700

    header file

---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif
