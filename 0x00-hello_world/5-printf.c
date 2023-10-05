f942e2a61ac5106fbb7618b04c9df291dc892206
anaazapata <cecizs1@hotmail.com>
2023-10-05 08:35:25 -0700

    use printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: use printf function
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
