2dccf5c267f4ebbbde45c1a2f354b29e4c898918
anaazapata <cecizs1@hotmail.com>
2023-10-05 08:47:25 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description: prints the size of various types on the computer it is compiled and run on
+ * Description: prints the size of various types on the
+ * computer it is compiled and run on
  * Return: Always 0
  */
 int main(void)
